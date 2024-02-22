4b0508fc35bf8b96c4a4815de6b7fa9b94eb2839
harishnkr <hari2menon1234@gmail.com>
2024-02-22 13:36:39 +0530

    added lualine characters function

--- lua/plugins/lualine/util.lua
+++ lua/plugins/lualine/util.lua
@@ -1,17 +1,33 @@
 local M = {}
 
+-- get number of words
+-- function M.getWords()
+--   if vim.bo.filetype == "text" or vim.bo.filetype == "markdown" then
+--     if vim.fn.wordcount().visual_words == 1 then
+--       return tostring(vim.fn.wordcount().visual_words) .. " word"
+--     elseif not (vim.fn.wordcount().visual_words == nil) then
+--       return tostring(vim.fn.wordcount().visual_words) .. " words"
+--     else
+--       return tostring(vim.fn.wordcount().words) .. " words"
+--     end
+--   else
+--     return ""
+--   end
+-- end
+
+-- get number of words and characters
 function M.getWords()
   if vim.bo.filetype == "text" or vim.bo.filetype == "markdown" then
-    if vim.fn.wordcount().visual_words == 1 then
-      return tostring(vim.fn.wordcount().visual_words) .. " word"
-    elseif not (vim.fn.wordcount().visual_words == nil) then
-      return tostring(vim.fn.wordcount().visual_words) .. " words"
-    else
-      return tostring(vim.fn.wordcount().words) .. " words"
-    end
+    local wordcount = vim.fn.wordcount()
+    local words = wordcount.visual_words or wordcount.words
+    local chars = wordcount.visual_chars or wordcount.chars
+    local wordStr = tostring(words) .. (words == 1 and " word" or " words")
+    local charStr = tostring(chars) .. (chars == 1 and " character" or " characters")
+    return wordStr .. ", " .. charStr
   else
     return ""
   end
 end
+
 
 return M
