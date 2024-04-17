9972924af3477433d637904d2d88edd5fb0b23ad
harishnkr <hari2menon1234@gmail.com>
2024-04-17 19:26:36 +0530

    add md support

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,16 @@
 -- lazy.nvim
 return {
-    "antonk52/markdowny.nvim",
-    config = function()
-        require('markdowny').setup()
-    end
+    {
+        "antonk52/markdowny.nvim",
+        enabled = false,
+        config = function()
+            require('markdowny').setup()
+        end
+    },
+    {
+        'quarto-dev/quarto-nvim',
+        sources = {name = 'otter'},
+    },{
+        'jmbuhr/otter.nvim',
+    }
 }
